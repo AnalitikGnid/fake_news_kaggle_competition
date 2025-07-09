49758008c4a79413c519e225de1d9a521a56f7c1
KalKoenkie <134587796+KalKoenkie@users.noreply.github.com>
2025-07-09 17:46:43 +0200

    Added plt import

--- helper_functions.py
+++ helper_functions.py
@@ -5,6 +5,7 @@ nltk.download('stopwords')
 # Import stopwords
 from nltk.corpus import stopwords
 from sklearn.metrics import roc_curve, auc, confusion_matrix
+import matplotlib.pyplot as plt
 
 def load_data(file_path):
     """
